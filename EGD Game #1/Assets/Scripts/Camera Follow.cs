85625edc0196ad423835f49763ea8fa60d86f898
lausan3 <lauanthony4455@gmail.com>
2023-10-04 15:24:27 -0400

    Almost done with player controller

--- EGD Game #1/Assets/Scripts/Camera Follow.cs
+++ EGD Game #1/Assets/Scripts/Camera Follow.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 public class Camera_Follow : MonoBehaviour
 {
     public Transform player;
-    public float cameraFollowSensitivity = 0.05f;
+    public float targetCameraFollowSensitivity = 0.02f;  // target lerpT when at initial base speed
+
+    private float lerpT;
+    private PlayerController pc;
+    private float initialSpeed;
+
+    private void Start()
+    {
+        pc = player.GetComponent<PlayerController>();
+
+        initialSpeed = pc.speed;
+    }
 
     private void Update()
     {
-        Vector2 pos = Vector2.Lerp(transform.position, player.transform.position, cameraFollowSensitivity);
+        Vector2 pos = Vector2.Lerp(transform.position, player.transform.position, lerpT);
         transform.position = new Vector3(pos.x, pos.y, -10f);
+    }
+
+    private void FixedUpdate()
+    {
+        // doing this so that as speed increases, the lerp increases as well as to not make the camera stray too far away
+        // lerpT = (speed / initialSpeed) * targetSensitivity
+        lerpT = targetCameraFollowSensitivity;
     }
 }
--- EGD Game #1/Assets/Scripts/PlayerController.cs
+++ EGD Game #1/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement")] 
-    public float speed = 1f;
-    
+    [Header("Speed")] 
+    private float speed;
 
     [Header("Combat")] 
+    public LayerMask enemyMask;
+    public KeyCode dashKey = KeyCode.Space;
+    public float IFrameTime = 0.05f;
+
+    private Vector2 aimingDirection;
+    private bool attacking;
+    // private Vector2 mousePos;
+    private Color originalColor;
+
+    [Header("Stats")] 
     public int startingHealth = 20;
+    public int damage = 10;
+    public float dashForce = 5f; // how powerful (how far) the dash is
+    public float dashCooldown = 0.5f; // how long it takes to dash again after dashing
+    public float knockbackForce = 5f; // how much the enemy moves away after being hit
+
     private int health;
-    
-    // Start is called before the first frame update
-    void Start()
+
+
+    #region Private references
+
+    private CircleCollider2D coli;
+    private Rigidbody2D rb;
+    private Material mat;
+
+    #endregion
+
+    private void Start()
     {
-        // Anthony - do we want linear translate movement or rigidbody force movement?
-        health = startingHealth;
+        attacking = false;
+        coli = GetComponent<CircleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        mat = GetComponent<Renderer>().material;
+        originalColor = mat.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
+        PlayerInput();
+        
+        // dash
+        if (Input.GetKeyDown(dashKey) && attacking == false)
+        {
+            StartCoroutine(Dash(dashCooldown));
+        }
     }
-    
-    // Move Player function
-    void MovePlayer()
+
+    private void PlayerInput()
     {
-        if (Input.GetKey(KeyCode.W)) { transform.Translate(Vector3.up * speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.A)) { transform.Translate(Vector3.left * speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.S)) { transform.Translate(Vector3.down * speed * Time.deltaTime); }
-        if (Input.GetKey(KeyCode.D)) { transform.Translate(Vector3.right * speed * Time.deltaTime); }
+        if (Input.GetKey(KeyCode.W))
+        {
+            // rb.AddForce(Vector2.up * speed * Time.deltaTime, ForceMode2D.Impulse);
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+            aimingDirection = Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // rb.AddForce(Vector2.left * speed * Time.deltaTime, ForceMode2D.Impulse);
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            aimingDirection = Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            // rb.AddForce(Vector2.down * speed * Time.deltaTime, ForceMode2D.Impulse);
+            transform.Translate(Vector2.down * speed * Time.deltaTime);
+            aimingDirection = Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            // rb.AddForce(Vector2.right * speed * Time.deltaTime, ForceMode2D.Impulse);
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            aimingDirection = Vector2.right;
+        }
+
+        // mouse calculations
+        // mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // aimingDirection = (mousePos - (Vector2)transform.position).normalized;
+    }
+
+    private IEnumerator Dash(float dashCooldown)
+    {
+        attacking = true;
+
+        // Physics2D.OverlapCircleAll()
+        float radius = (range - coli.radius) / 2;
+        // this is the range to use for OverlapCircleAll
+        Vector2 sendRange = (Vector2)transform.position + (aimingDirection * range) - (aimingDirection * new Vector2(radius, radius));
+        
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(sendRange, radius, enemyMask);
+        
+        foreach (var other in hitEnemies)
+        {
+            if (other != null)
+            {
+                IEnemy ec = other.transform.GetComponent<IEnemy>();
+                StartCoroutine(ec.Hurt(damage, knockbackForce));
+                // Debug.Log("Hurt " + other.name + "!");
+            }
+        }
+
+        float diameter = radius * 2;
+        attackType.transform.localScale = new Vector3(diameter, diameter, 1f);
+        
+        yield return new WaitForSeconds(dashCooldown);
+        
+        attacking = false;
+    }
+
+    public IEnumerator TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0f)
+        {
+            Debug.Log("Died!");
+            // start game over screen
+        }
+        
+        mat.color = Color.red;
+        
+        yield return new WaitForSeconds(IFrameTime);
+
+        mat.color = originalColor;
     }
 }
